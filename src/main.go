c142f4bca3d2f2abdbdddc9d32347d41a6bd537e
陈红光 <chg@meitu.com>
2018-12-07 17:37:18 +0800

    初始化代码

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/lealife/leacrawler"
+	// "os"
+)
+
+func main() {
+	start := time.Now()
+	fmt.Println("start...")
+
+	lea := leacrawler.NewCrawler()
+
+	url := "http://www.keenthemes.com/preview/metronic_admin"
+	path := "/Users/life/Desktop/LeaSpider"
+	lea.Fetch(url, path)
+
+	fmt.Printf("time cost %v\n", time.Now().Sub(start))
+}
